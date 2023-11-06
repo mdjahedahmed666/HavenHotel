06ef3ce73175d724f8ccf231e0f693db55b2ba1d
Jahed <mdjahedahmed666@gmail.com>
2023-11-06 01:36:27 +0000

    map section is added in home page

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,29 +1,29 @@
-// import GoogleMapReact from 'google-map-react';
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import GoogleMapReact from 'google-map-react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-// const Map = () => {
-//     const defaultProps = {
-//         center: {
-//           lat: 24.9048,
-//           lng: 91.8600
-//         },
-//         zoom: 11
-//       };
-//   return (
-//     <div style={{ height: '50vh', width: '100%' }}>
-//         <GoogleMapReact
-//         bootstrapURLKeys={{ key: "" }}
-//         defaultCenter={defaultProps.center}
-//         defaultZoom={defaultProps.zoom}
-//       >
-//         <AnyReactComponent
-//           lat={59.955413}
-//           lng={30.337844}
-//           text="My Marker"
-//         />
-//       </GoogleMapReact>
-//     </div>
-//   )
-// }
+const Map = () => {
+    const defaultProps = {
+        center: {
+          lat: 24.9048,
+          lng: 91.8600
+        },
+        zoom: 11
+      };
+  return (
+    <div style={{ height: '50vh', width: '100%' }}>
+        <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  )
+}
 
-// export default Map;
+export default Map;
