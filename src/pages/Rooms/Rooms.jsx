ebdd13dc09763c7498f531688ec590f72ee46ca2
Jahed <mdjahedahmed666@gmail.com>
2023-11-08 01:28:43 +0000

    Filter room is added

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -2,39 +2,75 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Rooms = () => {
-    const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(Number.MAX_VALUE);
 
-    useEffect(() => {
-      fetch("http://localhost:5000/rooms")
-        .then((res) => res.json())
-        .then((data) => setRooms(data));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/rooms")
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }, []);
+  const filterRoomsByPrice = () => {
+    return rooms.filter(
+      (room) => room.price_per_night >= minPrice && room.price_per_night <= maxPrice
+    );
+  };
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-3 py-10 container mx-auto px-4 md:px-32">
-      {rooms.map((room, index) => (
-        <div
-          key={index}
-          className="card card-compact bg-base-100 shadow-xl"
-        >
+    <div className="container mx-auto px-4 md:px-32 my-10">
+      <h2 className="font-bold font-rancho text-2xl">Filter Room by price range</h2>
+      <div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-base">Min Price:</span>
+          </label>
+          <input
+            type="number"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+            placeholder="Min Price:"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-base">Max Price:</span>
+          </label>
+          <input
+            type="number"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+            placeholder="Max Price:"
+            className="input input-bordered"
+          />
+        </div>
+      </div>
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-3 py-10">
+
+      {filterRoomsByPrice().map((room, index) => (
+        <div key={index} className="card card-compact bg-base-100 shadow-xl">
           <figure>
-            <img
-              src={room.room_image}
-              alt="Shoes"
-            />
+            <img src={room.room_image} alt="Shoes" />
           </figure>
           <div className="card-body">
             <h2 className="card-title font-rancho">{room.name}</h2>
             <p className="text-lg font-raleway">{room.description}</p>
-            <p className="text-lg font-raleway">Price: £{room.price_per_night}/night</p>
+            <p className="text-lg font-raleway">
+              Price: £{room.price_per_night}/night
+            </p>
             <p className="text-lg font-raleway">Total review: </p>
-            <Link to={`/rooms/${room.name}`} className="card-actions justify-end">
+            <Link
+              to={`/rooms/${room.name}`}
+              className="card-actions justify-end"
+            >
               <button className="btn btn-primary">Book Now</button>
             </Link>
           </div>
         </div>
       ))}
     </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Rooms;
